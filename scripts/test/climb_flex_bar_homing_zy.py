31a6e2a98bc1eb375ce66e3b3db65acf9f3402e9
zy-cuhk <zynwpu@hotmail.com>
2020-04-20 22:48:48 +0800

    adding some homing docuements

--- scripts/test/climb_flex_bar_homing_zy.py
+++ scripts/test/climb_flex_bar_homing_zy.py
@@ -30,84 +30,67 @@ def main():
         rospy.loginfo("%s is %s", rospy.resolve_name('write_flex_pole_motor_down'), write_flex_pole_motor_down)
         if home_climb_flex_bar==1:
 
-            os.system('rosparam set /renov_up_level/painting_oprea_over 0')
             os.system('rosparam set /renov_up_level/rotation_distance_tracking_over 0')
             os.system('rosparam set /renov_up_level/climb_distance_tracking_over 0')
 
+            # the downwards motion of flex pole 
+            os.system('rosparam set /renov_up_level/write_flex_pole_motor_down 2')
+            time.sleep(5)
+            rospy.loginfo("waiting for flex bar go down to start point")
+            os.system('rosparam set /renov_up_level/write_flex_pole_motor_down 0')
 
+            # reset motion of climb mechanism
             os.system('rosparam set /renov_up_level/enable_climb_control 1')
             os.system('rosparam set /renov_up_level/enable_climb_control 0')
             os.system('rosparam set /renov_up_level/open_climb_flag 0')
-            rospy.loginfo("waiting for rotation robot go  to start rad/degree")
+            rospy.loginfo("reset motion of climbing mechanism begin")
             os.system('rosparam set /renov_up_level/distance_climb_control 0')
-            rospy.set_param('distance_climb_control',0)
-            os.system('rosparam set /renov_up_level/open_climb_flag 1')
-            time.sleep(0.05)
-            os.system('rosparam set /renov_up_level/open_climb_flag 1')
-            time.sleep(6)
-            rospy.loginfo("waiting for climb robot go down to start point")
+            time.sleep(5)
+            rospy.loginfo("reset motion of climbing mechanism in process")
 
-
+            # reset motion of rotation mechanism 
             os.system('rosparam set /renov_up_level/enable_control_rotation 1')
             os.system('rosparam set /renov_up_level/enable_control_rotation 0')
-            rospy.set_param('rad_control_rotation',0)
-            os.system('rosparam set /renov_up_level/rad_control_rotation 0')
             os.system('rosparam set /renov_up_level/open_rotation_flag 1')
-            time.sleep(4)
-            
-
-            os.system('rosparam set /renov_up_level/distance_control_stand_bar 0')
-            time.sleep(0.05)
-            os.system('rosparam set /renov_up_level/distance_control_stand_bar 0')
-            os.system('rosparam set /renov_up_level/enable_second_control_stand_bar 1')
-            os.system('rosparam set /renov_up_level/open_hold_flag 1')
-            time.sleep(0.05)
-            os.system('rosparam set /renov_up_level/open_hold_flag 1')
-            time.sleep(15)
-
-
-            rospy.loginfo("waiting for stand bar go to start point")
-            os.system('rosparam set /renov_up_level/write_flex_pole_motor_down 2')
-            time.sleep(0.05)
-            os.system('rosparam set /renov_up_level/write_flex_pole_motor_down 2')
-            rospy.set_param('write_flex_pole_motor_down',2)
+            rospy.loginfo("reset motion of rotation mechanism begin")
+            os.system('rosparam set /renov_up_level/rad_control_rotation 0')
             time.sleep(5)
-            rospy.loginfo("waiting for flex bar go down to start point")
-            rospy.set_param('write_flex_pole_motor_down',0)
+            rospy.loginfo("reset motion of rotation mechanism in process")
 
+
+            # reset motion of standing bar 
+            os.system('rosparam set /renov_up_level/open_hold_flag 1')
+            os.system('rosparam set /renov_up_level/enable_control_stand_bar 1')
+            os.system('rosparam set /renov_up_level/enable_control_stand_bar 0')
+            os.system('rosparam set /renov_up_level/enable_second_control_stand_bar 1')
+            rospy.loginfo("reset motion of standing bar mechanism begin")
+            os.system('rosparam set /renov_up_level/distance_control_stand_bar 0')
+            time.sleep(5)
+            rospy.loginfo("reset motion of standing bar in process")
+
+            
             os.system('rosparam set /renov_up_level/open_climb_flag 0')
-            os.system('rosparam set /renov_up_level/write_flex_pole_motor_down 0')
-
-
-            os.system('rosparam set /renov_up_level/enable_climb_control 2')
-            time.sleep(0.05)
             os.system('rosparam set /renov_up_level/enable_climb_control 2')
             os.system('rosparam set /renov_up_level/enable_climb_control 0')
-
-            os.system('rosparam set /renov_up_level/enable_control_rotation 2')
             time.sleep(0.05)
+
+            os.system('rosparam set /renov_up_level/open_rotation_flag 0')
             os.system('rosparam set /renov_up_level/enable_control_rotation 2')
             os.system('rosparam set /renov_up_level/enable_control_rotation 0')
+            time.sleep(0.05)
 
-            
-
-            
+            os.system('rosparam set /renov_up_level/open_hold_flag 0')
+            os.system('rosparam set /renov_up_level/enable_control_stand_bar 2')
+            os.system('rosparam set /renov_up_level/enable_control_stand_bar 0')
+            os.system('rosparam set /renov_up_level/enable_second_control_stand_bar 0')
+            time.sleep(0.05)
            
+
             rospy.set_param('home_climb_flex_bar',0)
             os.system('rosparam set /renov_up_level/home_climb_flex_bar 0')
-            os.system('rosparam set /renov_up_level/open_rotation_flag 0')
-            os.system('rosparam set /renov_up_level/open_hold_flag 0')
-            os.system('rosparam set /renov_up_level/open_rotation_flag 0')
-            os.system('rosparam set /renov_up_level/enable_second_control_stand_bar 0')
-            os.system('rosparam set /renov_up_level/enable_control_rotation 0')
-
-
-
             rospy.set_param('mobile_tracking_stop_flag',0)
             os.system('rosparam set /renov_up_level/mobile_tracking_stop_flag 0')
             rospy.logerr("homing program over--  go to next mobile way point-------")
-            rospy.set_param('home_climb_flex_bar',0)
-            os.system('rosparam set /renov_up_level/home_climb_flex_bar 0')
             os.system('rosparam set /renov_up_level/hold_distance_tracking_over 0')
             os.system('rosparam set /renov_up_level/climb_distance_tracking_over 0')
             os.system('rosparam set /renov_up_level/rotation_distance_tracking_over 0')
